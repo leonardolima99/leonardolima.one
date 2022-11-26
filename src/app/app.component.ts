a04d0ef09a9e4ba3c3410e2118ca64a2ed1d4247
Leonardo Lima Coelho <dev.leonardolima@gmail.com>
2022-11-26 13:27:27 -0400

    feat: add initial style and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  prevent(e: SubmitEvent) {
+    e.preventDefault();
+  }
   title = 'leonardolima.one';
+  faBars = faBars;
 }
