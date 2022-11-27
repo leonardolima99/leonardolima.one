f5dd08978895226517c543d376b911880ce13c67
Leonardo Lima Coelho <dev.leonardolima@gmail.com>
2022-11-27 02:33:48 -0400

    feat: add responsivity to navbar, add new section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,26 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  prevent(e: SubmitEvent) {
+  prevent(e: SubmitEvent | MouseEvent) {
     e.preventDefault();
   }
   title = 'leonardolima.one';
   faBars = faBars;
+  linkActive: string = '';
+  menuActive: boolean = false;
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+  }
+  handleCloseMenu(event: MouseEvent) {
+    const hash = (event.target as HTMLAnchorElement).hash;
+    this.linkActive = hash;
+    if (hash === '') {
+      event.preventDefault();
+      window.location.hash = '';
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+    this.menuActive = false;
+  }
 }
